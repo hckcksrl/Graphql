ad23ba7d923fefa84c8d17e07094159e89f2d842
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-07-11 16:58:49 +0900

    Graphql Resolvers

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,15 @@
+import { people , getByID }from "./db"
+
+
 const resolvers = {
     Query : {
-        name : () => 'hckcksrl'
+        people : () => people,
+        /* 
+            resolver function is (parent, args, context, info) => {} 라서
+            _ 를 써서 parent를 사용안하게 하려고 쓰는것
+            설명해주는 주소 : https://github.com/prismagraphql/graphql-yoga/issues/137
+        */ 
+        person : (_,{id}) => getByID(id)
     }
 };  
 
