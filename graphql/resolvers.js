66c6d52d6fbefff590ee95b9cda5332e03580d25
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-07-11 19:17:34 +0900

    Graphql Resolvers

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,16 +1,29 @@
-import { people , getByID }from "./db"
+// import { getMovie , getByID , addMovie,deleteMovie }from "./db"
 
+
+// const resolvers = {
+//     Query : {
+//         movies : () => getMovie(),
+//         /* 
+//             resolver function is (parent, args, context, info) => {} 라서
+//             _ 를 써서 parent를 사용안하게 하려고 쓰는것
+//             설명해주는 주소 : https://github.com/prismagraphql/graphql-yoga/issues/137
+//         */ 
+//         movie : (_,{id}) => getByID(id)
+//     },
+//     Mutation : {
+//         addMovie : (_ , {name , score}) => addMovie(name , score),
+//         deleteMovie : (_,{id}) => deleteMovie(id)
+//     }
+// };  
+
+import {getMovie} from "./db"
 
 const resolvers = {
     Query : {
-        people : () => people,
-        /* 
-            resolver function is (parent, args, context, info) => {} 라서
-            _ 를 써서 parent를 사용안하게 하려고 쓰는것
-            설명해주는 주소 : https://github.com/prismagraphql/graphql-yoga/issues/137
-        */ 
-        person : (_,{id}) => getByID(id)
+        movies : (_,{limit , rating}) => getMovie(limit,rating)
     }
-};  
+}
+
 
 export default resolvers;
