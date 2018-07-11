b1a798d86c7ae676308a6d422eb5386a5a8ea439
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-07-11 19:26:13 +0900

    Use Axios

--- graphql/db.js
+++ graphql/db.js
@@ -53,20 +53,36 @@
 //     console.log(movies)
 //     return newMovie
 // }
+
 import fetch from "node-fetch"
+import { AlexaForBusiness } from "aws-sdk";
+import axios from "axios"
 
+const API_URL = "https://yts.am/api/v2/list_movies.json"
 
-const API_URL = "https://yts.am/api/v2/list_movies.json?"
+// export const getMovie = (limit , rating) => {
+//     let REQUEST_URL = API_URL;
+//     if(limit > 0){
+//         REQUEST_URL += `limit=${limit}`;
+//     }
+//     if(rating > 0){
+//         REQUEST_URL += `&minimum_rating=${rating}`;
+//     }
+//     return fetch(REQUEST_URL)
+//     .then(res => res.json())
+//     .then(json => json.data.movies);
+// }
 
-export const getMovie = (limit , rating) => {
-    let REQUEST_URL = API_URL;
-    if(limit > 0){
-        REQUEST_URL += `limit=${limit}`;
-    }
-    if(rating > 0){
-        REQUEST_URL += `&minimum_rating=${rating}`;
-    }
-    return fetch(REQUEST_URL)
-    .then(res => res.json())
-    .then(json => json.data.movies);
+export const getMovie = async (limit , rating) => {
+    const {
+        data: {
+            data : {movies}
+        }
+    } = await axios(API_URL, {
+        params : {
+            limit : limit,
+            minimum_rating : rating
+        }
+    })
+    return movies
 }
